0dd92712a629671ce84f5e8a61d84d5b869cb1a5
tesorerol <luistesorero5@gmail.com>
2021-08-18 22:52:52 -0500

    CriptoVision.sol

---
+++ CriptoVision.sol
@@ -0,0 +1,220 @@
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity ^0.8.5;
+
+abstract contract Context {
+    
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes memory) {
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        return msg.data;
+    }
+}
+
+interface BEP20 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+
+abstract contract Ownable is Context {
+    address private _owner;
+    address private _previousOwner;
+    bool private _lock=false;
+    string private mesageLock="";
+    mapping(address => bool) private _blacklist;
+    
+    constructor () {
+        _owner = _msgSender();
+    }
+
+    modifier isBlackedListed() {
+            require(_blacklist[_msgSender()]==false, "You are in the blacklist");
+            _;
+        }
+
+    modifier onlyOwner() {
+        require(_owner == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+    
+    modifier ContractLock() {
+        require(_lock == false, mesageLock);
+        _;
+    }
+    
+    //Locks the contract for owner for the amount of time provided
+    function lock(string memory mesage) external onlyOwner {
+        mesageLock=mesage;
+       _lock=true;
+    }
+    
+    function Getlock() public view  returns (bool) {
+        return _lock;
+    }
+    
+     function GetMesajeLock() public view  returns (string memory) {
+        return mesageLock;
+    }
+    //Unlocks the contract for owner when _lockTime is exceeds
+    function unlock() external onlyOwner {
+        _lock=false;
+        mesageLock="";
+    }
+    
+    function includeBlackListed(address account) external onlyOwner() {
+        require(_blacklist[account]==false, "Account is already in blacklist");
+        _blacklist[account] = true;
+    }
+    
+     function excludeBlackListed(address account) external onlyOwner() {
+        require(_blacklist[account]==true, "Account is already exclude from blacklist");
+        _blacklist[account] = false;
+    }
+  
+    function owner() public view returns (address) {
+        return _owner;
+    }
+    
+}
+
+contract CriptoVision is Context,BEP20, Ownable {
+    mapping (address => uint256) private _balanceOf;
+    mapping (address => mapping (address => uint256)) private _allowances;
+    string private _name = 'Cripto Vision';
+    string private _symbol = 'VSION';
+    uint8 private _decimals = 8;
+    
+    uint256 private _supply = 1000000000 * 10** _decimals;
+
+    constructor () {
+        _balanceOf[_msgSender()] = _supply;
+    }
+
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
+
+    function totalSupply() public view override returns (uint256) {
+        return _supply;
+    }
+      function balanceOf(address account) public view override returns (uint256) {
+        return _balanceOf[account];
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public view override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+
+    function approve(address spender, uint256 amount)  public override returns (bool) {
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        _transfer(sender, recipient, amount);
+        return true;
+    }
+
+
+    function _approve(address owner, address spender, uint256 amount) private isBlackedListed ContractLock {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+
+    function _transfer(address sender, address recipient, uint256 amount) private isBlackedListed ContractLock{
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(amount > 0, "Transfer amount must be greater than zero");
+        require(_balanceOf[sender] >= amount, "Balance too low");
+        _balanceOf[sender] -= amount;
+        _balanceOf[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+    }
+}
